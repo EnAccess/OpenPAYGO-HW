886cd3ccdb1cace0ca656b5efb307332b33484fd
benmod <david.bjm@gmail.com>
2019-09-25 14:59:49 +0200

    Also commented starting code

--- neutral_example_implentation/device_simulator/device_function_simulator.c
+++ neutral_example_implentation/device_simulator/device_function_simulator.c
@@ -6,10 +6,11 @@ uint16_t TokenCount = 1;
 bool PAYGEnabled = true;
 uint32_t ActiveUntil = 0;
 uint32_t TokenEntryLockedUntil = 0;
-// WARNING: THIS SECRET KEY IS ONLY HERE AS AN EXAMPLE AND SHOULD NEVER BE USED IN PRODUCTION
+// WARNING: THIS SECRET KEY AND STARTING CODE IS ONLY HERE AS AN EXAMPLE AND SHOULD NEVER BE USED IN PRODUCTION
+// uint32_t StartingCode = 123456789;
 //char SECRET_KEY[16] = {0xa2, 0x9a, 0xb8, 0x2e, 0xdc, 0x5f, 0xbb, 0xc4, 0x1e, 0xc9, 0x53, 0xf, 0x6d, 0xac, 0x86, 0xb1};
 char SECRET_KEY[16] = {...};
-uint32_t StartingCode = 123456789;
+
 
 // This is just for displaying the activation status in the simulater WaitForTokenEntry function
 extern bool IsActive(void);
